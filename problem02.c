25433efbe8b823d52d459eab6e4d4c370e5b8821
TAREQUE-AHMED <86972130+TAREQUE-AHMED@users.noreply.github.com>
2022-05-25 09:05:45 +0600

    commit init

---
+++ problem02.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+int c,d;
+printf("Enter the value you want:");
+scanf("%d",&c);
+printf("Enter the value you want:");
+scanf("%d",&d);
+if(c>d){
+    printf("c is bigger than d");
+}
+    else{
+        printf("d is bigger than c");
+    }
+
+getch();
+}
